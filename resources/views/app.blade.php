12fc23475e0217c0ecdcbb424e4290110b5543df
Gabriel L <gabrielleandrojunior@live.com>
2018-10-11 14:23:04 -0300

    Melhorias Build Prod

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,15 +6,15 @@
   <title>Frontend</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="dist/styles.css"/>
   <link rel="icon" type="image/x-icon" href="favicon.ico">
 </head>
 
 <body>
-  <app-root></app-root>
+  <app-root>Loading...</app-root>
   <script type="text/javascript" src="dist/runtime.js"></script>
   <script type="text/javascript" src="dist/polyfills.js"></script>
   <script type="text/javascript" src="dist/scripts.js"></script>
-  <script type="text/javascript" src="dist/styles.js"></script>
   <script type="text/javascript" src="dist/vendor.js"></script>
   <script type="text/javascript" src="dist/main.js"></script>
 </body>
