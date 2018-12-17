3a7c69611203e763898995cf3d77223f121a6aa3
Gabriel L <gabrielleandrojunior@live.com>
2018-12-17 20:54:12 -0200

    Update

---
+++ app/Jobs/SendDaySummaryJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace UniEventos\Jobs;
+
+use UniEventos\User;
+use UniEventos\Mail\DaySummary;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+
+class SendDaySummaryJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        User::all()->each(function(User $user){
+            
+        });
+    }
+}
--- app/Mail/DaySummary.php
+++ app/Mail/DaySummary.php
@@ -18,9 +18,9 @@ class DaySummary extends Mailable
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
